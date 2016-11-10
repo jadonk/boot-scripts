5c50513021d65a2db741a46bb48c5755d64bde4d
Jason Kridner <jdk@ti.com>
2016-11-10 10:57:49 -0500

    tools/eMMC/functions.sh: disable ext4 bad block test
    This is to speed up flashing in production. Revert this commit to restore bad block testing to address https://github.com/RobertCNelson/boot-scripts/pull/37.

--- tools/eMMC/functions.sh
+++ tools/eMMC/functions.sh
@@ -823,9 +823,9 @@ get_ext4_options(){
   LC_ALL=C mkfs.ext4 -V &> /tmp/mkfs
   test_mkfs=$(cat /tmp/mkfs | grep mke2fs | grep 1.43 || true)
   if [ "x${test_mkfs}" = "x" ] ; then
-    ext4_options="-c"
+    unset ext4_options
   else
-    ext4_options="-c -O ^metadata_csum,^64bit"
+    ext4_options="-O ^metadata_csum,^64bit"
   fi
 }
 
